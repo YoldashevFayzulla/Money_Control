f01bb2538df525da497ceeba2195624865ac0aa6
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-09-21 20:26:12 +0500

    make 2 role and routes finished

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -9,7 +10,16 @@ class IndexController extends Controller
 
     public function stratification()
     {
-          return dd('salom');
+        // dd('salom');
+
+
+        $user = AUTH::user();
+        if ($user -> hasRole('admin') ) {
+            return view('dashboard_admin');
+        }
+        if ($user -> hasRole('customer') ) {
+            return view('dashboard_customer');
+        }
     }
 
 }
